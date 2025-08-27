977775e375d3bb484dda8bc3a3494f4a0c1fe3d9
muhammad <karami8071@yahoo.com>
2025-08-27 20:33:54 +0330

    Added Seq Logging support

---
+++ Logging/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Common.Library.Settings;
+using Microsoft.Extensions.Logging;
+
+namespace Common.Library.Logging;
+public static class Extensions
+{
+    public static IServiceCollection AddSeqLogging(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddLogging(loggingBuilder =>
+        {
+            var seqSettings = config.GetSection(nameof(SeqSettings)).Get<SeqSettings>();
+            loggingBuilder.AddSeq(serverUrl: seqSettings.ServerUrl);
+        });
+        return services;
+    }
+}
